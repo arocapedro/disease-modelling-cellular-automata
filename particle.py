bca4b4daef0fd39f2243b96099d7415f76eedc00
parocalara <pedro.arocalara@ru.nl>
2022-06-19 17:42:41 +0200

    Add instructions, update gitignore, remove old notebook

--- particle.py
+++ particle.py
@@ -8,9 +8,9 @@ class Particle():
         self.step = 0
         self.state = State.SUSCEPTIBLE
 
-        self.remove_rate = 1/(np.random.uniform(remove_tuple[0], remove_tuple[1]))
-        self.latent_rate = 1/(np.random.uniform(latent_tuple[0], latent_tuple[1]))
-    
+        self.remove_rate = 1 / (np.random.uniform(remove_tuple[0], remove_tuple[1]))
+        self.latent_rate = 1 / (np.random.uniform(latent_tuple[0], latent_tuple[1]))
+
     def change_state(self, new_state):
         self.step = 0
         assert new_state in State.get_values(), f"Value {new_state} not found!"
